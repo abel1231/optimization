187781fc7ceada9ecca536870f9a20c667c2920b
tangyehui <yehuitang@sjtu.edu.cn>
2022-05-30 20:52:36 +0800

    first commit

--- test.py
+++ test.py
@@ -5,9 +5,32 @@ import time
 
 def oneCircuit(qlist, Hamiltonian, beta, gamma):
     vqc=VariationalQuantumCircuit()
+
+    for j in qlist:
+        vqc.insert(VariationalQuantumGate_RX(j,2.0*beta))
+
+    z_dict = []
+    zz_dict = []
+
     for i in range(len(Hamiltonian)):
+        tmp_vec = []
+        item = Hamiltonian[i]
+        dict_p = item[0]
+        for iter in dict_p:
+            if 'Z' != dict_p[iter]:
+                pass
+            tmp_vec.append(qlist[iter])
+
+        if 1 == len(tmp_vec):
+            z_dict.append(Hamiltonian[i])
+        elif 2 == len(tmp_vec):
+            zz_dict.append(Hamiltonian[i])
+        else:
+            raise AssertionError()
+
+    for i in range(len(z_dict)):
         tmp_vec=[]
-        item=Hamiltonian[i]
+        item=z_dict[i]
         dict_p = item[0]
         for iter in dict_p:
             if 'Z'!= dict_p[iter]:
@@ -16,18 +39,29 @@ def oneCircuit(qlist, Hamiltonian, beta, gamma):
 
         coef = item[1]
 
-        if 2 == len(tmp_vec):
-            vqc.insert(VariationalQuantumGate_CNOT(tmp_vec[0], tmp_vec[1]))
-            vqc.insert(VariationalQuantumGate_RZ(tmp_vec[1], 2*gamma*coef))
-            vqc.insert(VariationalQuantumGate_CNOT(tmp_vec[0], tmp_vec[1]))
-        elif 1 == len(tmp_vec):
+        if 1 == len(tmp_vec):
             vqc.insert(VariationalQuantumGate_RZ(tmp_vec[0], 2 * coef * gamma))
         else:
             raise AssertionError()
 
+    for i in range(len(zz_dict)):
+        tmp_vec = []
+        item = zz_dict[i]
+        dict_p = item[0]
+        for iter in dict_p:
+            if 'Z' != dict_p[iter]:
+                pass
+            tmp_vec.append(qlist[iter])
 
-    for j in qlist:
-        vqc.insert(VariationalQuantumGate_RX(j,2.0*beta))
+        coef = item[1]
+
+        if 2 == len(tmp_vec):
+            vqc.insert(VariationalQuantumGate_CNOT(tmp_vec[0], tmp_vec[1]))
+            vqc.insert(VariationalQuantumGate_RZ(tmp_vec[1], 2 * gamma * coef))
+            vqc.insert(VariationalQuantumGate_CNOT(tmp_vec[0], tmp_vec[1]))
+        else:
+            raise AssertionError()
+
     return vqc
 
 file_path = "./data/stock_data.xlsx"
@@ -36,12 +70,12 @@ exp_ret = exp_ret.to_numpy()
 cov_mat = cov_mat.to_numpy()
 
 
-budget = 1
+budget = 3
 # Gf = 1.0 / (2 ** (g - 1)) # granularity
-Gf = 1.0
-theta1 = Gf
-theta2 = 2.5 * Gf * Gf
-theta3 = 1.0
+Gf = 1 / 3.0
+theta1 = 1
+theta2 = 2.5
+theta3 = 1
 num_assets = 6
 epoch = 100
 
@@ -50,8 +84,8 @@ machine = init_quantum_machine(QMachineType.CPU)
 
 qlist = machine.qAlloc_many(num_qubit)
 
-J = (theta3 * budget**2 * Gf**2 + theta2 * cov_mat) / 4
-h = (-theta1 * exp_ret - 2*theta3* budget**2 * Gf) / 2 + np.sum(J, axis=1)
+J = (theta2 * cov_mat + theta3 * budget**2 * Gf**2) / 4
+h = (theta1 * exp_ret) / 2 + theta3 * budget**2 * Gf * (1-num_assets*Gf/2) - theta2 / 4 * (np.sum(cov_mat,axis=0) + np.sum(cov_mat,axis=1))
 
 problem = {}
 
@@ -77,8 +111,7 @@ for i in qlist:
     vqc.insert(VariationalQuantumGate_H(i))
 
 for i in range(step):
-    vqc.insert(oneCircuit(qlist,Hp.toHamiltonian(1),beta[i], gamma[i]))
-
+    vqc.insert(oneCircuit(qlist, Hp.toHamiltonian(1), beta[i], gamma[i]))
 
 loss = qop(vqc, Hp, machine, qlist)
 optimizer = MomentumOptimizer.minimize(loss, 0.01, 0.9)
@@ -90,7 +123,7 @@ loss_value_min = 0
 count = 0
 start = time.time()
 for i in range(epoch):
-    if count > 20:
+    if count > 100:
         break
     start_local = time.time()
     optimizer.run(leaves, 0)
@@ -109,7 +142,10 @@ print("Training done! Total elapsed time:{:.2f}s".format(time.time()-start))
 prog = QProg()
 qcir = vqc.feed()
 prog.insert(qcir)
+
 directly_run(prog)
+
+# print(convert_qprog_to_originir(prog, machine))
 
 result = quick_measure(qlist, int(1e6))
 print(sorted(result.items(), key = lambda kv:(kv[1], kv[0]), reverse=True))
